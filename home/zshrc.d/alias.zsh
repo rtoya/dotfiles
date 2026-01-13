b294dfafce6c8923a71a002ced9abfebd15388bd
rtoya <mshbmmsmsm.u.yauya.da.yo.n@gmail.com>
2026-01-13 17:18:41 +0900

    add alias to zshrc

--- home/zshrc.d/alias.zsh
+++ home/zshrc.d/alias.zsh
@@ -1,11 +1,22 @@
-alias ll='eza -la'
+alias e='exit'
+alias ll='ls -l'
 
 # git
+alias g='git'
+alias gb='git branch'
+alias gc='aicommits -a'
+alias gce='git commit --allow-empty -m "empty commit"'
+alias gco='git checkout'
 alias gf='git fetch'
 alias gg='git grep'
 alias gm='git merge'
-alias gpush='git push'
-alias gst='git status'
+alias gp='git push'
+alias gpush='gp origin $(gb --show-current)'
+alias gst='git status -s -b'
+alias gbd='git branch --merged | grep -Ev "main|master|develop" | xargs -I% git branch -D %'
+
+# aws
+alias asl='aws sso login --profile'
 
 # mise
 alias claude='mise exec -- claude'
