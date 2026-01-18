93068d4efa2711c83a12ad493403e1643a645aa0
rtoya <mshbmmsmsm.u.yauya.da.yo.n@gmail.com>
2026-01-18 16:53:49 +0900

    feat: add claude snippet and remove alias

--- home/modules/zsh/alias.zsh
+++ home/modules/zsh/alias.zsh
@@ -4,12 +4,5 @@ alias ls='eza'
 alias vim='nvim'
 alias vi='nvim'
 
-# git
-# alias gi='create-gitignore'
-# alias gc='aic'
-
 # kubernetes
 alias kubectl='kubecolor'
-
-# Claude Code
-alias claude='mise exec -- claude'
